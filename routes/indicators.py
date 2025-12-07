80097d640912b13fcf93f1324a5339bc1befe65f
Sara-Alr <s.a.i.alrassi@se25.qmul.ac.uk>
2025-12-07 22:24:07 +0000

    Update indicators.py
    Update routes to require authentication

--- routes/indicators.py
+++ routes/indicators.py
@@ -1,4 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from auth import require_api_key
+from models import Indicator, db
 
 indicators_bp = Blueprint("indicators", __name__)
 
+@indicators_bp.route("/indicators", methods=["GET"])
+@require_api_key
+def get_indicators():
+    indicators = Indicator.query.all()
+    data = [{"id": i.id, "code": i.code, "name": i.name} for i in indicators]
+    return jsonify(data)
