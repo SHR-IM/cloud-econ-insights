716ca5b322034d7a5e728af19343ec7f125150c7
Uhunoma Osemwengie <ec251025@cloud-econ-vm.europe-west2-c.c.rational-autumn-473710-p6.internal>
2025-12-13 18:19:47 +0000

    PHASE 9 DONE

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def create_app(config_class=DevConfig):
     return app
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+	app = create_app()
+	port = int(os.environ.get("PORT", 8080))
+	app.run(host="0.0.0.0", port=port, debug=True)
 
